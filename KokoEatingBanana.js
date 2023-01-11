7fe63f6b8869882ddc19ba4f775e2ec2e9d27c62
mohit kumar <mk7600000@gamil.com>
2023-01-11 18:15:44 +0530

    add 875. Koko Eating Bananas

---
+++ KokoEatingBanana.js
@@ -0,0 +1,25 @@
+// 875. Koko Eating Bananas
+
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+
+const calcTime = (piles, speed) =>
+  piles.reduce((acc, pile) => acc + Math.ceil(pile / speed), 0);
+
+var minEatingSpeed = function (piles, h) {
+  let l = 0,
+    r = Math.max(...piles),
+    mid,
+    time,
+    best;
+
+  while (l <= r) {
+    mid = Math.floor((l + r) / 2);
+    time = calcTime(piles, mid);
+    if (mid <= h) {
+      best = mid;
+      r = mid + 1;
+    } else l = mid - 1;
+  }
+  return best;
+};
